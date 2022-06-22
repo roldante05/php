4f358421f94d155984a4c81f03c4566fa6ab56ed
Dante Roldan <danteroldan72@gmail.com>
2022-06-22 11:36:23 -0300

    cambio

--- sistema_ventas/entidades/tipoproducto.php
+++ sistema_ventas/entidades/tipoproducto.php
@@ -1,29 +1,34 @@
 <?php
 
-class TipoProducto {
+class TipoProducto
+{
     private $idtipoproducto;
     private $nombre;
 
-    public function __construct(){
-
+    public function __construct()
+    {
     }
 
-    public function __get($atributo) {
+    public function __get($atributo)
+    {
         return $this->$atributo;
     }
 
-    public function __set($atributo, $valor) {
+    public function __set($atributo, $valor)
+    {
         $this->$atributo = $valor;
         return $this;
     }
 
 
-    public function cargarFormulario($request){
-        $this->idtipoproducto = isset($request["id"])? $request["id"] : "";
-        $this->nombre = isset($request["txtNombre"])? $request["txtNombre"] : "";
+    public function cargarFormulario($request)
+    {
+        $this->idtipoproducto = isset($request["id"]) ? $request["id"] : "";
+        $this->nombre = isset($request["txtNombre"]) ? $request["txtNombre"] : "";
     }
 
-    public function insertar(){
+    public function insertar()
+    {
         //Instancia la clase mysqli con el constructor parametrizado
         $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
         //Arma la query
@@ -42,20 +47,22 @@ class TipoProducto {
         $mysqli->close();
     }
 
-    public function actualizar(){
+    public function actualizar()
+    {
 
         $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
         $sql = "UPDATE tipo_productos SET
                 nombre = '$this->nombre'
                 WHERE idtipoproducto = " . $this->idtipoproducto;
-          
+
         if (!$mysqli->query($sql)) {
             printf("Error en query: %s\n", $mysqli->error . " " . $sql);
         }
         $mysqli->close();
     }
 
-    public function eliminar(){
+    public function eliminar()
+    {
         $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
         $sql = "DELETE FROM tipo_productos WHERE idtipoproducto = " . $this->idtipoproducto;
         //Ejecuta la query
@@ -65,7 +72,8 @@ class TipoProducto {
         $mysqli->close();
     }
 
-    public function obtenerPorId(){
+    public function obtenerPorId()
+    {
         $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
         $sql = "SELECT idtipoproducto, 
                         nombre
@@ -76,14 +84,14 @@ class TipoProducto {
         }
 
         //Convierte el resultado en un array asociativo
-        if($fila = $resultado->fetch_assoc()){
+        if ($fila = $resultado->fetch_assoc()) {
             $this->nombre = $fila["nombre"];
         }
         $mysqli->close();
-
     }
 
-    public function obtenerTodos(){
+    public function obtenerTodos()
+    {
         $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
         $sql = "SELECT 
                     idtipoproducto, 
@@ -94,9 +102,9 @@ class TipoProducto {
         }
 
         $aResultado = array();
-        if($resultado){
+        if ($resultado) {
             //Convierte el resultado en un array asociativo
-            while($fila = $resultado->fetch_assoc()){
+            while ($fila = $resultado->fetch_assoc()) {
                 $entidadAux = new TipoProducto();
                 $entidadAux->idtipoproducto = $fila["idtipoproducto"];
                 $entidadAux->nombre = $fila["nombre"];
@@ -105,5 +113,4 @@ class TipoProducto {
         }
         return $aResultado;
     }
-
 }
